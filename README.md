3e74cb1a0227733ad53ade0750c89db064fbaf42
Leonid Shevtsov <leonid@shevtsov.me>
2010-11-27 19:22:14 +0200

    fixed some shell script issues

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Not sure if these will be any use to anyone except me (or except as examples).
 
 To use this, run
 
-    bash < <( curl https://github.com/leonid-shevtsov/rails-templates/raw/master/rails.sh ) yourapp
+    bash -s < <( curl https://github.com/leonid-shevtsov/rails-templates/raw/master/rails.sh ) yourapp
--- rails.sh
+++ rails.sh
@@ -6,7 +6,9 @@ rvm gemset use $1
 gem install bundler
 gem install rails --version ">3"
 
-rails new $1 -skip-prototype --skip-testunit --database=mysql --template=~/rails_templates/rails_template.rb
+curl https://github.com/leonid-shevtsov/rails-templates/raw/master/rails_template.rb >> rails_template.$$.rb
+rails new $1 -skip-prototype --skip-testunit --database=mysql --template=rails_template.$$.rb
+rm rails_template.$$.rb
 
 cd $1
 
