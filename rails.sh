046d163dd8913f691310a81b7664a2ce9ba05583
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-22 14:28:40 +0200

    fixing input redirection

--- rails.sh
+++ rails.sh
@@ -3,8 +3,6 @@
 APP=$1
 TEMP_FILENAME="rails_template.$$.rb"
 
-exec < /dev/tty
-
 rvm gemset create $APP
 rvm gemset use $APP
 
@@ -13,6 +11,6 @@ gem install rails --version ">3"
 
 
 curl https://github.com/leonid-shevtsov/rails-templates/raw/master/rails_template.rb >> $TEMP_FILENAME
-rails new $APP -skip-prototype --skip-testunit --database=mysql --template=$TEMP_FILENAME
+rails new $APP -skip-prototype --skip-testunit --database=mysql --template=$TEMP_FILENAME </dev/tty
 
 echo "rvm gemset use $APP" >$APP/.rvmrc
