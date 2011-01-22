c69543f1bdf96c2c92c9fad920d40aae94ab159b
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-22 17:35:48 +0200

    fixed tempdir name

--- rails.sh
+++ rails.sh
@@ -2,7 +2,7 @@
 
 SOURCE_REPOSITORY="git://github.com/leonid-shevtsov/rails-templates.git'"
 APP=$1
-TEMP_DIR="rails_template.$$"i
+TEMP_DIR="rails_template.$$"
 
 rvm gemset create $APP
 rvm gemset use $APP
@@ -18,3 +18,5 @@ echo "rvm gemset use $APP" >$APP/.rvmrc
 cd $APP && git add .rvmrc && git commit -m "Added rvmrc"
 
 echo "rvm gemset trust $APP"
+
+#rm -rf $TEMP_DIR
