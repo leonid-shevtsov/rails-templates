838e7fcd8ee2b893325146cf08fdbe7820dac580
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-22 17:30:49 +0200

    changes rails.sh to use post_install

--- rails.sh
+++ rails.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+SOURCE_REPOSITORY="git://github.com/leonid-shevtsov/rails-templates.git'"
 APP=$1
-TEMP_FILENAME="rails_template.$$.rb"
+TEMP_DIR="rails_template.$$"i
 
 rvm gemset create $APP
 rvm gemset use $APP
@@ -9,9 +10,9 @@ rvm gemset use $APP
 gem install bundler
 gem install rails --version ">3"
 
-
-curl https://github.com/leonid-shevtsov/rails-templates/raw/master/rails_template.rb >> $TEMP_FILENAME
-rails new $APP -skip-prototype --skip-testunit --database=mysql --template=$TEMP_FILENAME </dev/tty
+git clone $SOURCE_REPOSITORY $TEMP_DIR
+rails new $APP -skip-prototype --skip-testunit --database=mysql --template=$TEMP_DIR/rails_template.rb
+cd $APP && ruby $TEMP_DIR/post_install.rb $APP ../$TEMP_DIR
 
 echo "rvm gemset use $APP" >$APP/.rvmrc
 cd $APP && git add .rvmrc && git commit -m "Added rvmrc"
